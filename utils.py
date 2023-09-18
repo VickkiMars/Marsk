0ed32d954fa7b663bef4260947f97477285dbba3
VickkiMars <victorumoreng@gmail.com>
2023-09-18 21:21:43 +0100

    Add imports and 'getAllData' method

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import math
+import datetime as dt
 import numpy as np
 import pandas as pd
 from tensorflow import keras
+from datetime import datetime, date
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
@@ -32,4 +34,31 @@ class stockPred:
     mse_thresh: float = .002,
     r2_thresh: float = .8
     ):
-    self.key1 = key1
+        self.key1 = key1
+        self.key2 = key2
+        self.history = datetime.timedelta(days = past_days)
+        self.trading_pair = trading_pair
+        self.exchange = exchange
+        self.feature = feature
+
+        self.look_back = look_back
+        self.split_perc = split_perc
+
+        #LSTM Model
+        self.neurons = neurons
+        self.activ_func = activ_func
+        self.dropout = dropout
+        self.loss = loss
+        self.optimizer = optimizer
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.output_size = output_size
+
+        self.plot = plot
+
+        # thresholds for model training
+        self.mse_thresh = mse_thresh
+        self.r2_thresh = r2_thresh
+
+    def getAllData(self):
+        data
